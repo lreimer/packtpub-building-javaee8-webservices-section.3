1771d6715d02209af67f657264623be453f29abd
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-02-19 10:46:10 +0100

    Template for Video 3.1

--- src/main/java/com/packtpub/javaee8/VersionResource.java
+++ src/main/java/com/packtpub/javaee8/VersionResource.java
@@ -6,8 +6,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Collections;
-import java.util.Map;
 
 @RequestScoped
 @Path("version")
@@ -16,25 +14,25 @@ public class VersionResource {
     /**
      * MediaType implementation for the version resource in v1.
      */
-    public static final MediaType V1 = new MediaType("application", "vnd.version.v1+json");
+    public static final MediaType V1 = null;
 
     /**
      * MediaType implementation for the version resource in v2.
      */
-    public static final MediaType V2 = new MediaType("application", "vnd.version.v2+json");
+    public static final MediaType V2 = null;
 
     @GET
-    @Produces("application/vnd.version.v2+json")
+    @Produces("TODO")
     public Response v2() {
-        Map<String, String> version = Collections.singletonMap("version", "v2");
-        return Response.ok(version).build();
+        // TODO Implement me
+        return null;
     }
 
     @GET
-    @Produces({"application/json; qs=0.75", "application/vnd.version.v1+json; qs=1.0"})
+    @Produces("TODO")
     public Response v1() {
-        Map<String, String> version = Collections.singletonMap("version", "v1");
-        return Response.ok(version).build();
+        // TODO Implement me
+        return null;
     }
 
 }
