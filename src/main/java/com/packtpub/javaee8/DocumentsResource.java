10d70e7e8059c3ae293460f1f4f0b8d3578e1bf8
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-02-19 10:16:44 +0100

    Implemented file upload for documents resource.

--- src/main/java/com/packtpub/javaee8/DocumentsResource.java
+++ src/main/java/com/packtpub/javaee8/DocumentsResource.java
@@ -1,20 +1,34 @@
 package com.packtpub.javaee8;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.servlet.ServletContext;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import java.io.File;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
-@ApplicationScoped
+import javax.enterprise.context.RequestScoped;
+import javax.servlet.ServletContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RequestScoped
 @Path("documents")
 public class DocumentsResource {
 
+    private static final Logger LOGGER = Logger.getAnonymousLogger();
+
     @Context
     private ServletContext context;
+    @Context
+    private UriInfo uriInfo;
 
     @GET
     @Path("/me.jpg")
@@ -32,5 +46,25 @@ public class DocumentsResource {
     public Response gif() {
         String path = context.getRealPath("/magic.gif");
         return Response.ok(new File(path)).build();
+    }
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response upload(@FormDataParam("file") InputStream inputStream,
+                           @FormDataParam("file") FormDataContentDisposition fileInfo) {
+        String fileName = fileInfo.getFileName();
+        saveFile(inputStream, fileName);
+
+        URI uri = uriInfo.getBaseUriBuilder().path(DocumentsResource.class).path(fileName).build();
+        return Response.created(uri).build();
+    }
+
+    private void saveFile(InputStream file, String name) {
+        try {
+            java.nio.file.Path path = FileSystems.getDefault().getPath("/tmp/", name);
+            Files.copy(file, path);
+        } catch (IOException ie) {
+            LOGGER.log(Level.WARNING, "Unable to save file.", ie);
+        }
     }
 }
--- src/main/java/com/packtpub/javaee8/JAXRSConfiguration.java
+++ src/main/java/com/packtpub/javaee8/JAXRSConfiguration.java
@@ -1,7 +1,10 @@
 package com.packtpub.javaee8;
 
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -9,4 +12,18 @@ import java.util.Set;
  */
 @ApplicationPath("api")
 public class JAXRSConfiguration extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+
+        classes.add(DocumentsResource.class);
+        classes.add(MultiPartFeature.class);
+
+        classes.add(HateosResource.class);
+        classes.add(JsonbResource.class);
+        classes.add(JsonpResource.class);
+        classes.add(VersionResource.class);
+
+        return classes;
+    }
 }
